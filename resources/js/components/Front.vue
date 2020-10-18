f0553f8189d15cb38bb783b7033b595e9053040a
Art Murr <12go12go@mail.ru>
2020-10-18 17:49:51 +0300

    Ini commit

---
+++ resources/js/components/Front.vue
@@ -0,0 +1,16 @@
+<template>
+    <h2>Front Page</h2>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component is mounted.')
+        },
+        name: "Front"
+    }
+</script>
+
+<style scoped>
+
+</style>
